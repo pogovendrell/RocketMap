67350982615ca079b63c978b99fc33cff62edc43
pkvndrll <pokevendrellmap@gmail.com>
2018-01-22 10:16:05 +0100

    Fixing merge problem

--- pogom/models.py
+++ pogom/models.py
@@ -226,25 +226,8 @@ class Pokemon(LatLongModel):
                             (Pokemon.longitude <= neLng))
                      .dicts())
 
-        # Performance:  disable the garbage collector prior to creating a
-        # (potentially) large dict with append().
-        gc.disable()
 
-        pokemon = []
-        for p in query:
-            p['pokemon_name'] = get_pokemon_name(p['pokemon_id'])
-            p['pokemon_rarity'] = get_pokemon_rarity(p['pokemon_id'])
-            p['pokemon_types'] = get_pokemon_types(p['pokemon_id'])
-            if args.china:
-                p['latitude'], p['longitude'] = \
-                    transform_from_wgs_to_gcj(p['latitude'], p['longitude'])
-            pokemon.append(p)
-
-
-        # Re-enable the GC.
-        gc.enable()
-
-        return pokemon
+        return list(query)
 
     @staticmethod
     @cached(cache)
